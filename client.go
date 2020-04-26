3889b6dab15040519185a683846996ad14df8499
Luke Stephens (hakluke) <13975395+hakluke@users.noreply.github.com>
2020-04-27 02:17:57 +1000

    added client, got it to a working state

---
+++ client.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+        "bufio"
+        "fmt"
+        "net"
+        "os/exec"
+        "strings"
+        "sync"
+        "time"
+        "github.com/alexflint/go-arg"
+        "strconv"
+)
+
+
+func main() {
+    var args struct {
+        Threads int     `arg:"-t" default:"5" help:"number of threads"`
+        Server string   `arg:"required" help:"server to connect to" placeholder:"IP:PORT"`
+        Password string `arg:"required" help:"password for server" placeholder:"PASSWORD"`   
+    }
+    arg.MustParse(&args)
+    
+    serverSplit := strings.Split(args.Server, ":")
+    serverIP := net.ParseIP(serverSplit[0])
+    port, _ := strconv.Atoi(serverSplit[1])
+    server := net.TCPAddr{IP: serverIP, Port: port} 
+    client := net.TCPAddr{IP: net.ParseIP("127.0.0.1")}
+    c, err := net.DialTCP("tcp", &client, &server)
+    for {
+        if err == nil {
+            break
+        } else {
+            fmt.Println("Retrying after error:",err)
+        }
+        time.Sleep(3000 * time.Millisecond)
+        c, err = net.DialTCP("tcp", &client, &server)
+    }
+
+    // set up workers
+    wg := &sync.WaitGroup{}
+    for i := 0; i <= args.Threads; i++ {
+        wg.Add(1)
+        go worker(wg, c, i)
+    }
+    
+    wg.Wait()
+}
+
+func worker(wg *sync.WaitGroup, c net.Conn, workerNumber int) {
+    defer wg.Done()
+    defer c.Close()
+    for {
+        //receive messages
+        message, _ := bufio.NewReader(c).ReadString('\n')
+        if message == "" {
+            time.Sleep(1000 * time.Millisecond)
+            continue
+        }
+        //trim the newline
+        message = strings.TrimSuffix(message, "\n")
+        fmt.Println("Worker", workerNumber, "executing:", message)
+        //execute the command
+        out, err := exec.Command("/bin/sh", "-c", message).Output()
+        if err != nil {
+                fmt.Println(err)
+        }
+        //print the command output
+        fmt.Printf("%s", out)
+    }
+}
--- server.go
+++ server.go
@@ -11,7 +11,7 @@ import (
 
 const (
     CONN_HOST = "localhost"
-    CONN_PORT = "1337"
+    CONN_PORT = 1337
     CONN_TYPE = "tcp"
 ) 
 
@@ -32,19 +32,21 @@ func main() {
         wg := &sync.WaitGroup{}
 
         // Listen for incoming connections.
-        l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+        listenIP := net.ParseIP(CONN_HOST)
+        host := net.TCPAddr{IP: listenIP, Port: CONN_PORT}
+        l, err := net.ListenTCP(CONN_TYPE, &host)
         if err != nil {
             fmt.Println("Error listening:", err.Error())
             os.Exit(1)
         }
 
-        // Clost the listener when the application closes.
+        // Close the listener when the application closes.
         defer l.Close()
         fmt.Println("Listening on", CONN_HOST, ":", CONN_PORT)
 
         for {
             // Listen for an incoming connection.
-            conn, err := l.Accept()
+            conn, err := l.AcceptTCP()
             if err != nil {
                 fmt.Println("Error accepting:", err.Error())
                 os.Exit(1)
@@ -61,7 +63,7 @@ func handleRequest(work chan string, wg *sync.WaitGroup, conn net.Conn) {
     defer wg.Done()
     for text := range work {
         // write the job to the connection
-        conn.Write([]byte(text + "\n"))
+        conn.Write([]byte(text + string('\n')))
         // print a message saying what the message was and where it went
         fmt.Println("Sending to", conn.RemoteAddr().String(), ":", text)
     }
